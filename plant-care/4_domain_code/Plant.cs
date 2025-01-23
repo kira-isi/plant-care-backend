c144aa0e832cf98f59c92ecbb4a3733f84764d19
Fabian Hagenmeier <fabian.hagenmeier@targomed.de>
2025-01-23 11:38:45 +0100

    initial setup of domain code layer, some todos still open

---
+++ plant-care/4_domain_code/Plant.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace plant_care._4_domain_code
+{
+	internal class Plant
+	{	
+		private Guid plantID {get; set;}
+		private String name { get; set; }
+		private String type { get; set; }
+		private float requiredWaterAmount { get; set; }
+		private int dailyWateringTimes { get; set; }
+		private Boolean needsDirectSunlight { get; set; }
+		private Boolean keepOutside { get; set; }
+
+		public Plant(Guid plantID, string name, string type, float requiredWaterAmount, int dailyWateringTimes, bool needsDirectSunlight, bool keepOutside)
+		{
+			this.plantID = plantID;
+			this.name = name;
+			this.type = type;
+			this.requiredWaterAmount = requiredWaterAmount;
+			this.dailyWateringTimes = dailyWateringTimes;
+			this.needsDirectSunlight = needsDirectSunlight;
+			this.keepOutside = keepOutside;
+		}
+
+		
+	}
+}
