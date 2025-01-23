58947a52d4d516c9ef19ebb437b4c0dccd789684
Fabian Hagenmeier <fabian.hagenmeier@targomed.de>
2025-01-23 11:38:45 +0100

    initial setup of domain code layer, some todos still open

---
+++ plant-care/4_domain_code/Action.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace plant_care._4_domain_code
+{ 
+	internal abstract class Action
+	{
+		public String name { get; set; }
+		public String description { get; set; }
+		public Action(String name, String description) {
+			this.name = name;	
+			this.description = description;
+
+		}
+	}
+}
